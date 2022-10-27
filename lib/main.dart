22d15fa4fca4a22c1816dbd899e6be410e9fc58a
user.salmawafaa <user.salma1905060@miuegypt.edu.eg>
2022-10-27 20:07:52 +0200

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sahar_mob_app/pages/contact_us.dart';
 import 'package:sahar_mob_app/pages/edit_account.dart';
 import 'package:sahar_mob_app/pages/login_page.dart';
 import 'package:sahar_mob_app/pages/regi_page.dart';
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: AddProductPage(),
+      home: ContactUs(),
       //home: CategoryPage(),
     );
   }
