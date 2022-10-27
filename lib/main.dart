f60f0a9703dd6b7fda62e792616ea7bbd1b8e646
rogina <rogina.michelle6@icloud.com>
2022-10-27 17:00:23 +0200

    my cart

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      // home: Shop(),
       home: AddProductPage(),
       //home: CategoryPage(),
     );
