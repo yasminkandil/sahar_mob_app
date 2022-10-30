702f7e5ca064cb267492fead54d276fe70ed7a0c
rogina <rogina.michelle6@icloud.com>
2022-10-30 18:10:12 +0200

    cart

---
+++ lib/ProductModel.dart
@@ -0,0 +1,12 @@
+import 'package:sahar_mob_app/screens/ProductScreen.dart';
+import 'package:flutter/material.dart';
+
+class ProductModel {
+  String name;
+  int price;
+
+  ProductModel(String name, int price) {
+    this.name = name;
+    this.price = price;
+  }
+}
