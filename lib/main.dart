8deecee184eb732711961944ab244704c4cd8d08
yasminkandil <yasminkandil2001@gmail.com>
2022-10-30 18:13:06 +0200

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: SafeArea(child: AddOfferPage()),
+      home: SafeArea(child: LoginPage()),
     );
   }
 }
