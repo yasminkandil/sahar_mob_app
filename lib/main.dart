7a05e05f21dc96fb8e275804e0bbec793e61b5fd
salmawafaa <salma1905060@miuegypt.edu.eg>
2022-10-30 22:06:48 +0200

    Navbar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:sahar_mob_app/pages/contact_us.dart';
 import 'package:sahar_mob_app/pages/edit_account.dart';
 import 'package:sahar_mob_app/pages/login_page.dart';
+import 'package:sahar_mob_app/pages/navbar.dart';
 import 'package:sahar_mob_app/pages/regi_page.dart';
 import 'package:sahar_mob_app/pages/add_product.dart';
 import 'package:sahar_mob_app/pages/category.dart';
@@ -42,53 +43,7 @@ class MyApp extends StatelessWidget {
       // home: Shop(),
      // home: AddProductPage(),
       //home: CategoryPage(),
-      home: SafeArea(child: ViewAccountPage()),
-    );
-  }
-}
-
-class CartApp extends StatefulWidget {
-  @override
-  _CartAppState createState() => _CartAppState();
-}
-
-class _CartAppState extends State<CartApp> {
-  List<ProductModel> cart = [];
-  int sum = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Cart App"),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                text: "Products",
-              ),
-              Tab(
-                text: "Checkout",
-              ),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            ProductScreen((selectedProduct) {
-              setState(() {
-                cart.add(selectedProduct); //update
-                sum = 0;
-                cart.forEach((item) {
-                  sum = sum + item.price;
-                });
-              });
-            }),
-            CheckoutScreen(cart, sum),
-          ],
-        ),
-      ),
+      home: SafeArea(child: Navigation_bar()),
     );
   }
 }
