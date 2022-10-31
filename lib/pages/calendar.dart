84722ad48d2128066b3101173d47d57bf7d76081
karamella55 <youssef1903816@miuegypt.edu.eg>
2022-10-31 05:20:59 +0200

    handeling

--- lib/pages/calendar.dart
+++ lib/pages/calendar.dart
@@ -1,19 +1,19 @@
-import 'package:syncfusion_flutter_calendar/calendar.dart';
-import 'package:flutter/material.dart';
+// import 'package:syncfusion_flutter_calendar/calendar.dart';
+// import 'package:flutter/material.dart';
 
-class CalendarPage extends StatefulWidget {
-  const CalendarPage({super.key});
+// class CalendarPage extends StatefulWidget {
+//   const CalendarPage({super.key});
 
-  @override
-  CalendarState createState() => CalendarState();
-}
+//   @override
+//   CalendarState createState() => CalendarState();
+// }
 
-class CalendarState extends State<CalendarPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: SfCalendar(
-      view: CalendarView.month,
-    ));
-  }
-}
+// class CalendarState extends State<CalendarPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         body: SfCalendar(
+//       view: CalendarView.month,
+//     ));
+//   }
+// }
