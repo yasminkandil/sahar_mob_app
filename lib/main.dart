9b0a57a57d52bf87b4417ea00c0db94d6d36657f
salmawafaa <salma1905060@miuegypt.edu.eg>
2022-12-21 12:24:57 +0200

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:sahar_mob_app/pages/navbar.dart';
 
 //import 'pages/cart_view.dart';
 //import 'pages/calendar.dart';
-
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+ 
   @override
   Widget build(BuildContext context) {
+   
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'cart page',
@@ -19,7 +21,31 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: SafeArea(child: Navigation_bar()),
+      home: const SafeArea(child: HomePage()),
     );
   }
+}
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+ 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Future<FirebaseApp> _initializeFirebase() async{
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+    return firebaseApp;
+  }
+  @override
+  Widget build(BuildContext context) {
+ return Scaffold(
+      body: FutureBuilder(future: _initializeFirebase(),
+      builder:((context, snapshot) {
+        if(snapshot.connectionState==ConnectionState.done)
+        {
+          return Navigation_bar();
+        }
+        return const Center(child: CircularProgressIndicator(),);
+      }) , ),);  }
 }
