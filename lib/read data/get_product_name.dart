42a3f55dced15e78bf056ececeb95d82f7d219b6
salmawafaa <salma1905060@miuegypt.edu.eg>
2022-12-25 02:33:04 +0200

    ..

--- lib/read data/get_product_name.dart
+++ lib/read data/get_product_name.dart
@@ -1,36 +1,62 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-
 class GetProductName extends StatelessWidget {
-
   // const GetUserName({super.key});
-final String salma;
- GetProductName({required this.salma});
+  final String salma;
+  GetProductName({required this.salma});
   @override
-
   Widget build(BuildContext context) {
+    CollectionReference products =
+        FirebaseFirestore.instance.collection('products');
+  final pro = FirebaseFirestore.instance.collection('products');
 
-    CollectionReference products=
-    FirebaseFirestore.instance.collection('products');
-    
-    
-    return FutureBuilder<DocumentSnapshot>
-    (
+    return FutureBuilder<DocumentSnapshot>(
       future: products.doc(salma).get(),
-            builder:((context, snapshot){
+      builder: ((context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data = snapshot.data?.data() != null
+              ? snapshot.data!.data()! as Map<String, dynamic>
+              : <String, dynamic>{};
+          return RichText(
+              text: TextSpan(
+                    style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold), //apply style to all
+text: 'Product Number: ${data['ID']}\n',
+                  children: [
+                TextSpan(
+                    text: 'Name : ',
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold)),
+                TextSpan(
+                    text: '${data['name']}\n',
+                    style: TextStyle(
+                        color: Colors.deepOrange,
+                        fontWeight: FontWeight.normal)) ,
 
 
-       if (snapshot.connectionState == ConnectionState.done) {
-         Map<String, dynamic> data = 
-             snapshot?.data?.data()!=null?
-             snapshot?.data!.data()! 
-              as Map<String,dynamic>:<String,dynamic>{}; 
-             return Text('Name : ${data['name']}');
+                TextSpan(
+                  
+                    text: 'Description  : ',
+                    style: TextStyle(
+                   color: Colors.black, fontWeight: FontWeight.bold)),
+                
+                TextSpan(
+                    text: '${data['description']}\n',
+                    style: TextStyle(
+                        color: Colors.deepOrange,
+                        fontWeight: FontWeight.normal)),
+              ]));
+
+          /* ');
+                 Text (${da
+                 ta['title']}
+             
+             ', Price : ${data['price']}'
+             ', Description : ${data['description']}');*/
         }
-       return Text('loading..'); 
-    }),
+        return Text('loading..');
+      }),
     );
-  
   }
+ 
 }
