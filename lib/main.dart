0e2034468b5924dc63a1b39310d026d308cdc531
r0gina <rogina.michelle1@gmail.com>
2023-01-15 18:34:17 +0200

    dark&light mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,47 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/services.dart';
+//import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sahar_mob_app/home/home_page.dart';
 
+import 'package:flutter/cupertino.dart';
 import 'package:sahar_mob_app/home/navbar.dart';
+import 'package:sahar_mob_app/providers/themeprovider.dart';
+import 'package:provider/provider.dart';
+//import 'package:sahar_mob_app/pages/theme_data.dart';
 
 //import 'pages/cart_view.dart';
 //import 'pages/calendar.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(MyApp());
+  //runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Sahar',
-      theme: ThemeData(
+    return ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: Consumer<ThemeProvider>(
+        builder: (context, theme, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Sahar',
+            theme: theme.getTheme(),
+            darkTheme: ThemeData.dark(),
+
+            /*ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),*/
+            home: const SafeArea(child: MyHomePage()),
+          );
+        },
       ),
-      home: const SafeArea(child: MyHomePage()),
     );
   }
 }
