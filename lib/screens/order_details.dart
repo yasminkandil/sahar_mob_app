b33cf4b85f9feab668b583ddd6980421af562d33
karamella55 <youssef1903816@miuegypt.edu.eg>
2023-01-15 20:11:16 +0200

    order history

--- lib/screens/order_details.dart
+++ lib/screens/order_details.dart
@@ -1,27 +1,67 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:sahar_mob_app/screens/order_status.dart';
 
 class OrdersDetails extends StatelessWidget {
-  const OrdersDetails({super.key, this.data});
-  final dynamic data;
+  const OrdersDetails({super.key, required this.salma});
+  final String salma;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
         appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 61, 61, 61),
           title: Text("My Orders Details"),
         ),
-        body: Column(
-          children: [
-            orderStatus(
-                color: Colors.orange,
-                icon: Icons.done,
-                title: "Order Placed",
-                showdone: data['orderStatus']),
-          ],
-        ));
+        backgroundColor: Colors.orange,
+        body: FutureBuilder(
+            future: FirebaseFirestore.instance
+                .collection('orders')
+                .doc(salma)
+                .get(),
+            builder: ((context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                Map<String, dynamic> data = snapshot.data?.data() != null
+                    ? snapshot.data!.data()! as Map<String, dynamic>
+                    : <String, dynamic>{};
+                return ListView(
+                  children: [
+                    Text("Email:  ${data['orderBy']} ",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            height: 2)),
+                    Text("Date: ${data['orderDate']}",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            height: 2)),
+                    Text("Order Status: ${data['orderStatus']}",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            height: 2)),
+                    Text(
+                      "Product Name: ${data['products']}",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 18, height: 2),
+                    ),
+                    Text("Total Paid:${data['Total']}" + "EGP",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            height: 2)),
+                    Text("Paument Method: ${data['paymentMethod']}",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            height: 2)),
+                  ],
+                );
+              }
+              return Text("Loading...");
+            })));
   }
 }
