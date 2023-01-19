121c163204dec5a2cae5e5d429187ae3b536b270
yasminkandil <yasminkandil2001@gmail.com>
2023-01-19 23:13:01 +0200

    alertt

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -236,6 +236,12 @@ class _BodyState extends State<Body> {
                                                           .doc(val.id)
                                                           .update(
                                                               {"id": val.id});
+                                                    }).then((value) {
+                                                      Fluttertoast.showToast(
+                                                          msg:
+                                                              "Product added to cart sucessfully ✔️",
+                                                          backgroundColor:
+                                                              GreyLightColors);
                                                     });
                                                   } else {
                                                     int oldPrice = contain
@@ -263,13 +269,13 @@ class _BodyState extends State<Body> {
                                                         .update({
                                                       "count": newCount,
                                                       "totalPrice": newPrice,
+                                                    }).then((value) {
+                                                      Fluttertoast.showToast(
+                                                          msg:
+                                                              "Product added to cart sucessfully ✔️",
+                                                          backgroundColor:
+                                                              GreyLightColors);
                                                     });
-
-                                                    Fluttertoast.showToast(
-                                                        msg:
-                                                            "Product added to cart sucessfully ✔️",
-                                                        backgroundColor:
-                                                            Colors.red);
                                                   }
                                                 });
                                               } else {
